f1e3a7523f33219af7e972b39a7c1e6fcf069cec
khushi-saxena <khushisaxena123123@gmail.com>
2026-03-28 21:17:47 -0700

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Khushi Saxena on 3/28/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskManagerTests/TaskManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerTests.swift
+//  TaskManagerTests
+//
+//  Created by Khushi Saxena on 3/28/26.
+//
+
+import Testing
+@testable import TaskManager
+
+struct TaskManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
